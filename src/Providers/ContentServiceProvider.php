9f440a77384aca48ba47ca01b9e91f3f8dd7bcc6
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-26 18:09:24 +0200

    Add composer.json and ContentServiceProvider.php

---
+++ src/Providers/ContentServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Latus\Content\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
