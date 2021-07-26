7468c6d4a12e73c1851819e8febd3080cd8b5cc2
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-26 19:40:55 +0200

    Add bindings for ContentRepository.php and ContentTranslationRepository.php contracts

--- src/Providers/ContentServiceProvider.php
+++ src/Providers/ContentServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Latus\Content\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Latus\Content\Repositories\Contracts\ContentRepository;
+use Latus\Content\Repositories\Contracts\ContentTranslationRepository;
 
 class ContentServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,13 @@ class ContentServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if (!$this->app->bound(ContentRepository::class)) {
+            $this->app->bind(ContentRepository::class, \Latus\Content\Repositories\Eloquent\ContentRepository::class);
+        }
 
+        if (!$this->app->bound(ContentTranslationRepository::class)) {
+            $this->app->bind(ContentTranslationRepository::class, \Latus\Content\Repositories\Eloquent\ContentTranslationRepository::class);
+        }
     }
 
     /**
