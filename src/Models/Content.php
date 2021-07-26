3e4c9cb7fc78d743c1c686a57837861d9ef72242
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-26 18:25:20 +0200

    Add Content.php model and migration

---
+++ src/Models/Content.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Latus\Content\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Content extends Model
+{
+    protected $fillable = [
+        'type', 'name', 'owner_model_id', 'owner_model_class', 'title', 'text'
+    ];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo($this->owner_model_class, 'owner_model_id');
+    }
+    
+}
