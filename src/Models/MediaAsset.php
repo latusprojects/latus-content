ad5b4010be3795b441e01e1a8b0fa285b049a0e1
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-16 00:08:36 +0200

    Add MediaAsset.php model and migration

---
+++ src/Models/MediaAsset.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Latus\Content\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class MediaAsset extends Model
+{
+    protected $fillable = [
+        'is_public', 'name', 'filename', 'description', 'owner_model_id', 'owner_model_class', 'reference'
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    protected $attributes = [
+        'description' => ''
+    ];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo($this->owner_model_class, 'owner_model_id');
+    }
+}
